e7450ece7b3c68a2d22eb28a4994a8617a870240
Cyle Witruk <236413682+cylewitruk-stacks@users.noreply.github.com>
2026-07-10 08:23:30 -0700

    make NakamotoBlock::txs public

--- stackslib/src/chainstate/nakamoto/mod.rs
+++ stackslib/src/chainstate/nakamoto/mod.rs
@@ -834,7 +834,7 @@ impl FromRow<NakamotoBlockHeader> for NakamotoBlockHeader {
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct NakamotoBlock {
     pub header: NakamotoBlockHeader,
-    pub(crate) txs: Vec<StacksTransaction>,
+    pub txs: Vec<StacksTransaction>,
 }
 
 impl NakamotoBlock {
